ff13312b9f92bcc43b8945cd4e8b856d559041f9
Temba Sydney Gabriel <tembasydney@gmail.com>
2024-07-10 21:20:19 +0300

    fsixth commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './components/HomePage';
 import GamePage from './components/GamePage';
-import MoviesPage from './components/MoviesPage';
+import Navbar from './components/NavBar';
 import Cart from './components/Cart';
 import Login from './components/Login';
 import OrderHistory from './components/OrderHistory';
@@ -11,15 +11,17 @@ function App() {
     return (
         <BrowserRouter>
             <div>
+                <Navbar/>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/games" element={<GamePage />} />
-                    <Route path="/movies" element={< MoviesPage/>} />
+                    
                     <Route path="/cart" element={<Cart />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/order-history" element={<OrderHistory />} />
                 </Routes>
             </div>
+
         </BrowserRouter>
     );
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,11 +5,12 @@ function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
       <Link to="/games">Games</Link>
-      <Link to="/movies">Movies</Link>
       <Link to="/cart">Cart</Link>
       <Link to="/login">Login</Link>
       <Link to="/order-history">Order History</Link>
+      <Link to="/contacts">Contacts</Link>
     </nav>
   );
 }
