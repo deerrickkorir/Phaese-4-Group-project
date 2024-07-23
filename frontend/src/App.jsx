ae82ec85500f3d9e00ad5173c331f5422be91bb6
kennedy <kitongakennedy9@gmail.com>
2024-07-23 19:24:00 +0300

    landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,32 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from './components/HomePage';
-import GamePage from './components/GamePage';
-import Navbar from './components/NavBar';
-import Cart from './components/Cart';
-import Login from './components/Login';
-import AboutPage from './components/About';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Contact from './components/Contact';
-import AddGame from './components/AddGame';
-
+import HomePage from "./components/HomePage";
+import GamePage from "./components/GamePage";
+import Navbar from "./components/NavBar";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import AboutPage from "./components/About";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contact from "./components/Contact";
+import AddGame from "./components/AddGame";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div>
-                <Navbar/>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/games" element={<GamePage />} />
-                    <Route path="/contacts" element={<Contact />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/add game" element={<AddGame />} />
-                    <Route path="/about" element={<AboutPage />} /> 
-                </Routes>
-            </div>
-
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/games" element={<GamePage />} />
+          <Route path="/contacts" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/add game" element={<AddGame />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
-
-
