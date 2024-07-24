1c2c35af69166aa15a361b73565440f70ba0dc18
Isaac <isaacmusera05@gmail.com>
2024-07-24 11:24:26 -0700

    final changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './components/HomePage';
 import GamePage from './components/GamePage';
@@ -6,28 +6,32 @@ import Navbar from './components/NavBar';
 import Cart from './components/Cart';
 import Login from './components/Login';
 import AboutPage from './components/About';
-import SignUp from './components/SignUp'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contact from './components/Contact';
 import AddGame from './components/AddGame';
+import { ThemeProvider } from './ThemeContext';
+import { Toaster } from 'react-hot-toast';
+
 
 function App() {
-    return (
+    return ( <ThemeProvider>
         <BrowserRouter>
             <div>
-                <Navbar />
+            <Toaster/> 
+                <Navbar/>
                 <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/home" element={<HomePage/>} />
+                    <Route path="/" element={<Login/>} />
+                    <Route path="/home" element={<HomePage />} />
                     <Route path="/games" element={<GamePage />} />
                     <Route path="/contacts" element={<Contact />} />
                     <Route path="/cart" element={<Cart />} />
-                    <Route path="/signUp" element={<SignUp />} /> {/* Add the SignUp route */}
                     <Route path="/add game" element={<AddGame />} />
-                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/about" element={<AboutPage />} /> 
                 </Routes>
             </div>
+
         </BrowserRouter>
+        </ThemeProvider>
     );
 }
 
